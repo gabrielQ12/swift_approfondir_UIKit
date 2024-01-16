6fe3ace5bb4f57e56359fa537f16de97897b160e
gabrielQ12 <gabriel.quinette1@gmail.com>
2024-01-16 07:56:30 +0100

    cycle de vie en pratique

---
+++ approfondir_UIKit/approfondir_UIKit/approfondir_UIKit/SecondController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondController.swift
+//  approfondir_UIKit
+//
+//  Created by Gaby on 16/01/2024.
+//
+
+import UIKit
+
+class SecondController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Second view")
+    }
+}
--- approfondir_UIKit/approfondir_UIKit/approfondir_UIKit/ViewController.swift
+++ approfondir_UIKit/approfondir_UIKit/approfondir_UIKit/ViewController.swift
@@ -9,10 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        print("Coder")
+    }
+    
+    override func loadView() {
+        super.loadView()
+        print("Load View")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("View didLoad")
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("Will Appear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("View did appear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("View will desapear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("View did disapear")
+    }
+
 
 
 }
