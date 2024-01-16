9fb2508fc155a89ef177bc1867e857349e188778
gabrielQ12 <gabriel.quinette1@gmail.com>
2024-01-16 14:43:31 +0100

    CGRect

--- approfondir_UIKit/approfondir_UIKit/approfondir_UIKit/SecondController.swift
+++ approfondir_UIKit/approfondir_UIKit/approfondir_UIKit/SecondController.swift
@@ -13,4 +13,30 @@ class SecondController: UIViewController {
         super.viewDidLoad()
         print("Second view")
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        createView()
+    }
+    
+    func createView() {
+        //X
+        let x: CGFloat = 69
+        //Y
+        let y: CGFloat = 134
+        //Width
+        let width: CGFloat = 200
+        //Height
+        let heigth: CGFloat = 128
+        
+        let size: CGSize = CGSize(width: width, height: heigth)
+        let point: CGPoint = CGPoint(x: x, y: y)
+        
+        let rect = CGRect(origin: point, size: size)
+        
+        // let rect: CGRect = CGRect (x: x, y: y, width: width, height: heigth)
+        let v: UIView = UIView(frame: rect)
+        v.backgroundColor = .systemRed
+        self.view.addSubview(v)
+    }
 }
